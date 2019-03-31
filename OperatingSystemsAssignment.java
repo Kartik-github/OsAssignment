c40ed402082c8b71e7c5ca33e4bc18f8bb43d568
Kartik-github <32673278+Kartik-github@users.noreply.github.com>
2019-03-31 23:07:04 +0530

    Added Comments

--- OperatingSystemsAssignment.java
+++ OperatingSystemsAssignment.java
@@ -2,13 +2,11 @@ class Osproject
 {
   public static void main(String[] args)
   {
-    double page_fault_service_time=8,EAT=200,memory_access_time=200,page_fault_rate;
-    page_fault_service_time=(0.7)*20+0.3*8;
-    page_fault_rate=((EAT-100)/page_fault_service_time)*Math.pow(10,-6);
-    System.out.println("The minimum page fault rate="+page_fault_rate);
-    System.out.println("Which is equal to=6.01 x 10^-9");
+    double page_fault_service_time=8,EAT=200,memory_access_time=200,page_fault_rate; //initialise predefined values
+    page_fault_service_time=(0.7)*20+0.3*8;                                          //Calculating page fault service time
+    page_fault_rate=((EAT-100)/page_fault_service_time)*Math.pow(10,-6);             //Calculating the page fault rate
+    System.out.println("The minimum page fault rate="+page_fault_rate);              //Displaying the Result
+    System.out.println("Which is equal to=6.01 x 10^-9");                            //Displaying the rounded off understandable result
 
   }
 }
-//Description
-/**/
