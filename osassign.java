e92ca63f66863598938a55d540c6b7d9de07e9d7
Kartik-github <32673278+Kartik-github@users.noreply.github.com>
2019-03-31 11:55:16 +0530

    Add files via upload

---
+++ osassign.java
@@ -0,0 +1,14 @@
+class Oskaproject
+{
+  public static void main(String[] args)
+  {
+    double page_fault_service_time=8,EAT=200,memory_access_time=200,page_fault_rate;
+    page_fault_service_time=(0.7)*20+0.3*8;
+    page_fault_rate=((EAT-100)/page_fault_service_time)*Math.pow(10,-6);
+    System.out.println("The minimum page fault rate="+page_fault_rate);
+    System.out.println("Which is equal to=6.01 x 10^-9");
+
+  }
+}
+//Description
+/**/
